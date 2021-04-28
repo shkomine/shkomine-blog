a5514cb581b2e295865471d8ce5c1861950dab57
shkomine <sh.5i.boc@gmail.com>
2021-04-28 10:06:22 +0900

    ✨ contact to profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,7 +2,7 @@ import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 
 import sharedStyles from '../styles/shared.module.css'
-import contactStyles from '../styles/contact.module.css'
+import profileStyles from '../styles/profile.module.css'
 
 import GitHub from '../components/svgs/github'
 import Twitter from '../components/svgs/twitter'
@@ -20,36 +20,28 @@ const contacts = [
     alt: 'github icon',
     link: 'https://github.chkomine',
   },
-  // {
-  //   Comp: LinkedIn,
-  //   alt: 'linkedin icon',
-  //   link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  // },
-  // {
-  //   Comp: Envelope,
-  //   alt: 'envelope icon',
-  //   link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
-  // },
 ]
 
-export default function Contact() {
+export default function Profile() {
   return (
     <>
-      <Header titlePre="Contact" />
+      <Header titlePre="Profile" />
       <div className={sharedStyles.layout}>
-        <div className={contactStyles.avatar}>
+        <div className={profileStyles.avatar}>
           <img src="/avatar.jpg" alt="avatar drawing" height={60} />
         </div>
 
-        <h1 style={{ marginTop: 0 }}>Contact</h1>
+        <h1 style={{ marginTop: 0 }}>Profile</h1>
 
-        <div className={contactStyles.name}>
+        <div className={profileStyles.name}>
           shkomine - プロダクト駆動エンジニア/小さな会社のCTO
-          <br />
+        </div>
+        <div className={profileStyles.profile}>
           ご連絡は、TwitterのDMにてよろしくお願いします。
+          <br />
         </div>
 
-        <div className={contactStyles.links}>
+        <div className={profileStyles.links}>
           {contacts.map(({ Comp, link, alt }) => {
             return (
               <ExtLink key={link} href={link} aria-label={alt}>
