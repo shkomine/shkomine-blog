4244fead1a9e0244ffefc7b700d42ec0bfdb2057
shkomine <sh.5i.boc@gmail.com>
2021-04-28 10:06:07 +0900

    ✨ drop source code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,8 +7,7 @@ import styles from '../styles/header.module.css'
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  { label: 'Profile', page: '/profile' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -19,12 +18,9 @@ const Header = ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
-        <meta
-          name="description"
-          content="An example Next.js site using Notion for the blog"
-        />
-        <meta name="og:title" content="My Notion Blog" />
+        <title>{titlePre ? `${titlePre} |` : ''} shkomine blog</title>
+        <meta name="description" content="" />
+        <meta name="og:title" content="shkomine blog" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
         <meta name="twitter:card" content="summary_large_image" />
